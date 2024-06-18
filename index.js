32179c2809b8eb4da1a76758ab70e251efe14b90
AnimeshTaff <animesh.kumar@taffinc.com>
2024-06-18 19:36:49 +0530

    main file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require('./routes/shopify');
 app.use(cors());
 app.use('/shopify', routes);
 
-const port = 3000;
+const port = 8000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
